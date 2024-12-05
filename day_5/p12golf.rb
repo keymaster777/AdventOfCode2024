848a82b7e27c4a3c287e9265d0fb53bc2a957bd0
Riley Cleary <rjc@pop-os.localdomain>
2024-12-05 10:29:41 -0700

    Adds part1 and part2 golf combo

---
+++ day_5/p12golf.rb
@@ -0,0 +1,1 @@
+l=File.readlines("i.t");s=l.index("\n");r,u,s,z=l[0..s],l[s+1..-1].map{|e|e.chop.split(",")},0,0;u.each{|p|c=0;(d=c;r.map{|l|a,b=l.chop.split("|").map{|e|p.index(e)};p[a],p[b],c=p[b],p[a],c+1 if a&&b&&a>b};break if d==c)while 1;h=p[p.size/2].to_i;c==0?z+=h:s+=h};p z;p s
