6f7ab31576ad2458f6c3609e02d6541c3c85ac7d
Riley Cleary <rjc@pop-os.localdomain>
2024-12-05 08:42:15 -0700

    removing some spaces from day 5 part1 golf

--- day_5/part_1_golf.rb
+++ day_5/part_1_golf.rb
@@ -1,1 +1,1 @@
-l=File.readlines("i.t");s=l.index("\n");r,u,s=l[0..s],l[s+1..-1],0;u.each{|p|next unless r.each{|m|a,b=m.strip.split("|").map{|e| p.index(e)};break if a && b && a > b};a=p.split(",");s+=a[a.size/2].to_i};p s
+l=File.readlines("i.t");s=l.index("\n");r,u,s=l[0..s],l[s+1..-1],0;u.each{|p|next unless r.each{|m|a,b=m.strip.split("|").map{|e|p.index(e)};break if a&&b&&a>b};a=p.split(",");s+=a[a.size/2].to_i};p s
